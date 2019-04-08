67e83aa5d8b1b6102e0acfb111855db840b09bfb
Vineet S Kamat <vk284@cornell.edu>
2019-04-08 12:31:22 -0400

    Add files via upload

---
+++ authors.mli
@@ -0,0 +1,12 @@
+(**
+   Metadata about this submission.
+   @author
+   Mike Park (jp848)
+   Mueed Ur Rehman (mr2265)
+   Rishi Advani (ra534)
+   Vineet Kamat (vk284)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
