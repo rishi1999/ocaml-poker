d5d324b853a2f562cc9c9103762c218576f794be
Vineet S Kamat <vk284@cornell.edu>
2019-04-08 23:01:06 -0400

    added mli description for main

---
+++ main.ml
@@ -0,0 +1,1 @@
+let play_game = failwith "unimplemented"
---
+++ main.mli
@@ -0,0 +1,9 @@
+(** [play_game f] plays the game with the file with name [f].
+    The user inputs commands to control the game.
+    Specifications of these commands are in [command.mli].
+    Requires: [f] is a text file that contains game state.
+    If the data is in an invalid format,
+    or if [f] does not exist, an exception will be thrown.
+    Example: [play_game poker.txt] plays the game with [poker.txt].
+*)
+val play_game : string -> unit
