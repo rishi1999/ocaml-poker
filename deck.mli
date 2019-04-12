b1316852a962e06c82e41865d9920c6f5a8d4672
Vineet S Kamat <vk284@cornell.edu>
2019-04-11 20:29:12 -0400

    added couple functions for table  and updated deck mli

--- deck.mli
+++ deck.mli
@@ -1,1 +1,24 @@
+(**Type representing the rank of a card*)
 type rank
+
+(**Type representing the suit of a card*)
+type suit
+
+(**Type representing a card*)
+type card
+
+
+val shuffle_deck : unit
+
+val shuffle_list : ((suit * rank) * int -> (suit * rank) * int -> int) -> 'a -> (suit * rank) list
+
+val pick_card : suit * rank
+
+val pick_cards : int -> (suit * rank) list
+
+val update_state : (suit * rank) list -> unit
+
+val deck_size : int
+
+val deck_init : unit
+
--- table.ml
+++ table.ml
@@ -1,9 +1,9 @@
 (*Keeps track of players, dealers, and blinds*)
+open Deck
 
-
-type player = string
+type player = {name: string; cards: (Deck.suit * Deck.rank) list}
 type players = player list
-type table = {dealer: int; blind: int; participants: players; hole_cards: int list}
+type table = {dealer: int; blind: int; participants: players; hole_cards: (Deck.suit * Deck.rank) list}
 
 
 
@@ -15,3 +15,21 @@ let next_round_players table = function
   |{dealer = d; blind = b; participants = p; hole_cards = c}
     -> {dealer = d; blind = b; participants = p; hole_cards = c}
 
+
+let deal (table:table)=
+  Deck.deck_init;
+  Deck.shuffle_deck;
+  let deal_helper = function
+    | {name = s; cards = []} when List.length c < 2
+      -> {name = s; cards = Deck.pick_cards 2}
+    | _ -> failwith "player issue"
+  in
+  let rec deal_to_each players list=
+    match players with
+    | [] -> list
+    | player::t -> deal_to_each t ((deal_helper player)::list)
+
+  in
+  match table with
+  |{dealer = d; blind = b; participants = p; hole_cards = c} 
+    -> {dealer = d; blind = b; participants = (deal_to_each p []); hole_cards = c}
