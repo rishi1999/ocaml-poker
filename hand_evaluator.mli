9ba21e4edd76e5e0afea4deaf3897350ef2a3fce
Vineet S Kamat <vk284@cornell.edu>
2019-04-16 18:33:39 -0400

    Update hand_evaluator.mli

--- hand_evaluator.mli
+++ hand_evaluator.mli
@@ -5,3 +5,7 @@
     Requires hand has 7 cards
     throws "not 7 cards" exception if hand does not have 7 cards*)
 val seven_list_eval : (Deck.suit * Deck.rank) list -> int
+
+(** [get_nth (lst, n)] returns the nth value of lst
+      Requires n is less than length of list - 1*)
+val get_nth : 'a list * int -> 'a
