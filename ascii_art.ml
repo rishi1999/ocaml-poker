1ae15dca0f374fb351d04d57f6a4690828826bdf
Vineet S Kamat <vk284@cornell.edu>
2019-04-17 17:34:33 -0400

    ascii

--- ascii_art.ml
+++ ascii_art.ml
@@ -1,3 +1,7 @@
+open Deck
+type ascii_card = Deck.suit * Deck.rank * string
 
-let suits_name = ["Spades"; "Diamonds"; "Hearts"; "Clubs"]
+let rank_symbols = ["A";"2";"3";"4";"5";"6";"7";"8";"9";"10";"J";"Q";"K"]
 let suits_symbols = ["♠";"♦";"♥";"♣"]
+
+
