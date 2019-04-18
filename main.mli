cf929f6e52001aee50e4c15b15b1243c04e854a3
mr2265 <mr2265@cornell.edu>
2019-04-17 23:33:35 -0400

    updated

--- main.mli
+++ main.mli
@@ -4,6 +4,9 @@
     Requires: [f] is a text file that contains game state.
     If the data is in an invalid format,
     or if [f] does not exist, an exception will be thrown.
-    Example: [play_game poker.txt] plays the game with [poker.txt].
-*)
+    Example: [play_game poker.txt] plays the game with [poker.txt]. *)
 val play_game : State.t -> unit
+
+(*Note to Grader: The interface was designed with a dark terminal
+  background color in mind. Visibility of certain printed cards is reduced
+  if the terminal background color is white. *)
