5515352d874f38863e105f9787c375a7456fa6c4
Mike Park <jp848@cornell.edu>
2019-04-17 23:26:21 -0400

    player documentation

--- player.ml
+++ player.ml
@@ -5,9 +5,6 @@ type player =
     money: int;
   }
 
-(** [id player] returns the id of the player.
-    Requires: player is a valid player.
-    Example: [id {}  *)
 let id player = player.id
 let cards player = player.cards
 let money player = player.money
--- player.mli
+++ player.mli
@@ -1,10 +1,21 @@
+(** [id] : the id of the player
+    [cards] : the cards that the player is currently holding
+    [money] : the amount of money the player currently has
+*)
 type player =
   {
     id: int;
     cards: (Deck.suit * Deck.rank) list;
     money: int;
   }
-
+(** [id player] returns the id of the player.
+    Requires: player is a valid player. *)
 val id : player -> int
+
+(** [cards player] returns the cards that the player has.
+    Requires: player is a valid player. *)
 val cards : player -> (Deck.suit * Deck.rank) list
+
+(** [money player] returns how much the player has.
+    Requires: player is a valid player. *)
 val money : player -> int
