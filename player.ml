c1ceca29448c4dd9cf4d80a6a8fe5bb66ea9a41b
Rishi Advani <ra534@cornell.edu>
2019-04-20 18:53:23 -0400

    rewrote/fixed documentation for all of player.mli

--- player.ml
+++ player.ml
@@ -1,19 +1,12 @@
 type player =
   {
     id: int;
+    name: string;
     cards: Deck.card list;
     money: int;
   }
 
 let id player = player.id
-
-(** [id player] returns the id of the player.
-    Requires: [player] is a valid player.
-    Example: cards {id = 0; cards = [(Diamonds, Ace)]; money = 5} is 
-    [(Diamonds, Ace)] *)
+let name player = player.name
 let cards player = player.cards
-
-(** [money player] returns the id of the player.
-    Requires: [player] is a valid player.
-    Example: money {id = 0; cards = []; money = 5} is 5 *)
 let money player = player.money
--- player.mli
+++ player.mli
@@ -5,17 +5,30 @@
 type player =
   {
     id: int;
+    name: string;
     cards: Deck.card list;
     money: int;
   }
+
 (** [id player] returns the id of the player.
-    Requires: player is a valid player. *)
+    Requires: [player] is a valid player.
+    Example: [id {... id = 3; ...}] is
+    [3]. *)
 val id : player -> int
 
-(** [cards player] returns the cards that the player has.
-    Requires: player is a valid player. *)
+(** [name player] returns the name of the player.
+    Requires: [player] is a valid player.
+    Example: [name {... name = "Nate"; ...}] is
+    ["Nate"]. *)
+val name : player -> string
+
+(** [cards player] returns the name of the player.
+    Requires: [player] is a valid player.
+    Example: [cards {... cards = [(Diamonds, Ace)]; ...}] is
+    [[(Diamonds, Ace)]]. *)
 val cards : player -> Deck.card list
 
-(** [money player] returns how much the player has.
-    Requires: player is a valid player. *)
+(** [money player] returns the id of the player.
+    Requires: [player] is a valid player.
+    Example: [money {... money = 5; ...}] is [5]. *)
 val money : player -> int
