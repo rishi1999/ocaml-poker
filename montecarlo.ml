937cd8d52bd61d1be1924d4528f005b8dad50c50
Rishi Advani <ra534@cornell.edu>
2019-04-24 18:55:16 -0400

    fixed formatting in montecarlo.ml docs

--- montecarlo.ml
+++ montecarlo.ml
@@ -3,7 +3,7 @@ open Hand_evaluator
 open State
 open Player
 
-let community_card_fill base_cards used = 
+let community_card_fill base_cards used =
   let number_needed = 5 - List.length (base_cards) in
   let required_cards = pick_new number_needed used in
   base_cards @ required_cards
@@ -11,8 +11,8 @@ let community_card_fill base_cards used =
 (* PyPoker Engine stuff *)
 let montecarlo_simulation num_players hole board =
   let community_card = community_card_fill board (hole @ board) in
-  let unused_card = pick_new ((num_players - 1) * 2) (hole @ community_card) in 
-  let rec hand_builder current_hand count hand_list card_list = 
+  let unused_card = pick_new ((num_players - 1) * 2) (hole @ community_card) in
+  let rec hand_builder current_hand count hand_list card_list =
     match card_list with
     |  [] -> List.rev hand_list
     | h :: t  when count = 1 -> let new_hand = h :: current_hand in
@@ -24,28 +24,28 @@ let montecarlo_simulation num_players hole board =
     | h :: t -> let hand_score = seven_list_eval (h @ community_card) in
       opponent_rank_builder (hand_score :: outlist) t in
   let opponent_rank = opponent_rank_builder [] opponent_hands in
-  let min_opponent_rank = List.fold_left (fun accu elem -> min accu elem) 
+  let min_opponent_rank = List.fold_left (fun accu elem -> min accu elem)
       7463 opponent_rank in
   let my_score = seven_list_eval (hole @  community_card) in
   if my_score < min_opponent_rank then 1.0 else 0.0
 
 (* estimate_win_rate *)
 let estimate_win_rate num_simulations num_player hole board =
-  let rec win_count sum count = 
-    if count = 0 then sum 
-    else let win = montecarlo_simulation num_player hole board in 
+  let rec win_count sum count =
+    if count = 0 then sum
+    else let win = montecarlo_simulation num_player hole board in
       win_count (sum +. win) (count - 1) in
   let wins = win_count 0.0 num_simulations in
   let float_simulations = float_of_int num_simulations in
   wins /. float_simulations
 
-let declare_action bot hole_cards state iterations = 
+let declare_action bot hole_cards state iterations =
   let valid_actions = state.avail_action in
-  let win_rate = estimate_win_rate iterations (state.num_players) hole_cards 
+  let win_rate = estimate_win_rate iterations (state.num_players) hole_cards
       (state.table.board) in
   let can_call = List.mem "call" valid_actions in
   let call_amount = if can_call then calculate_pay_amt state else 0 in
-  if bot.money = 0 then 
+  if bot.money = 0 then
     ("check", 0)
   else
   if win_rate >= 0.85 then ("raise", bot.money)
@@ -59,18 +59,18 @@ let declare_action bot hole_cards state iterations =
     The number of cards returned is equal to 5 - the number of elements
     in [base_cards].
     Example: [community_card_fill_eff [1;2;3;5] [1;2;3;4;6;7] could be
-    [4]. 
+    [4].
     Requires: [base_cards] and [used_cards] to be lists containing integers
     from 0 (inclusive) to 51 (inclusive). All elements of [base_cards]
-    must be insde [used_cards].*)
-let community_card_fill_eff base_cards used = 
+    must be inside [used_cards]. *)
+let community_card_fill_eff base_cards used =
   let number_needed = 5 - List.length (base_cards) in
   let required_cards = pick_efficient number_needed used in
   base_cards @ required_cards
 
 (** [montecarlo_simulation_efficient num_players hole board] runs
     a montecarlo simulation for a bot with current hand [hole] and
-    visible community cards [board]. [num_players] - 1 hands are simulated to
+    visible community cards [board]. [num_players - 1] hands are simulated to
     represent the hands of the players other than the bot that are in the
     game. If [hole] and [board] form a better hand than the hands randomly
     chosen for the other players with the same [board], the functions 
@@ -82,7 +82,7 @@ let community_card_fill_eff base_cards used =
     on the random opponent hands that are simulated.
     Requires: [hole] is an int list containing 2 cards represented as integers
     from 0 (inclusive) to 51 (inclusive), the integers in [hole]
-    and those in [board]] must be distinct from each other and [num_players]]
+    and those in [board] must be distinct from each other and [num_players]
     must be a positive value. *)
 let montecarlo_simulation_efficient num_players hole board =
   let community_card = community_card_fill_eff board (hole @ board) in
@@ -118,7 +118,7 @@ let montecarlo_simulation_efficient num_players hole board =
     and the seed values are randomly initialized.
     Requires: [hole] is an int list containing 2 cards represented as integers
     from 0 (inclusive) to 51 (inclusive), the integers in [hole]
-    and those in [board]] must be distinct from each other and [num_players]]
+    and those in [board] must be distinct from each other and [num_players]
     must be a positive value. *)
 let estimate_win_rate_eff num_simulations num_player hole board =
   let int_hole = List.map int_converter hole in
@@ -169,7 +169,7 @@ let declare_action_eff bot hole_cards state iterations =
     on the random opponent hands that are simulated.
     Requires: [hole] is an int list containing 2 cards represented as integers
     from 0 (inclusive) to 51 (inclusive), the integers in [hole]
-    and those in [board]] must be distinct from each other and [num_players]]
+    and those in [board] must be distinct from each other and [num_players]
     must be a positive value. *)
 let mcs_2p hole board =
   let community_card = community_card_fill_eff board (hole @ board) in
@@ -191,7 +191,7 @@ let mcs_2p hole board =
     and the seed values are randomly initialized.
     Requires: [hole] is an int list containing 2 cards represented as integers
     from 0 (inclusive) to 51 (inclusive), the integers in [hole]
-    and those in [board]] must be distinct from each other. *)
+    and those in [board] must be distinct from each other. *)
 let estimate_win_rate_mcs_2p num_simulations hole board =
   let int_hole = List.map int_converter hole in
   let int_board = List.map int_converter board in
@@ -244,10 +244,10 @@ let declare_action_2p bot hole_cards state iterations =
     if call_amount = -1 then
       ("check", 0)
     else
-      if (call_amount < bot.money / 30) then
-        ("call", call_amount)
-      else
-        ("fold", 0)
+    if (call_amount < bot.money / 30) then
+      ("call", call_amount)
+    else
+      ("fold", 0)
   else if (List.mem "check" valid_actions) && call_amount = -1
-    then ("check", 0) 
-  else ("fold",0) 
+  then ("check", 0)
+  else ("fold",0)
