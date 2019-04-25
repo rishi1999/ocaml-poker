17deb6d79afd443376c185c6e14e4a042d7c40e1
mr2265 <mr2265@cornell.edu>
2019-04-24 22:47:57 -0400

    updated

--- main.mli
+++ main.mli
@@ -9,4 +9,8 @@ val play_game : State.t -> unit
 
 (*Note to Grader: Please maximize the terminal window before
   running make play in order to have the best experience and
-  avoid any issues with the UI not fully displaying.. *)
+  avoid any issues with the UI not fully displaying. We designed
+  this for a native OCaml installation on a 15 inch laptop
+  screen running a maximized terminal. If you use a smaller terminal,
+  the ASCII art may not display correctly and you have to scroll to
+  see the full UI for each new turn in the game. *)
